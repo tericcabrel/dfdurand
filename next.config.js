9fea450f0bb183a4229aecff0e1f053abb4a74a0
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2025-12-07 18:52:56 +0100

    Upgrade next (#3)
    * fix: upgrade version
    
    * fix: update videos

--- next.config.js
+++ next.config.js
@@ -1,21 +1,20 @@
-const { withContentlayer } = require('next-contentlayer');
+const { withContentlayer } = require("next-contentlayer");
 
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = withContentlayer({
-  swcMinify: true,
-  images: {
-    domains: [
-      'pbs.twimg.com',
-      'vercel.com',
-      'i.ytimg.com',
-      'images.unsplash.com',
-      'og-image-react-egghead.vercel.app',
-      'images.ctfassets.net',
-      'secure.meetupstatic.com',
-      'cdn.changelog.com',
-      'speakeasyjs.com',
-    ],
-  },
+	images: {
+		domains: [
+			"pbs.twimg.com",
+			"vercel.com",
+			"i.ytimg.com",
+			"images.unsplash.com",
+			"og-image-react-egghead.vercel.app",
+			"images.ctfassets.net",
+			"secure.meetupstatic.com",
+			"cdn.changelog.com",
+			"speakeasyjs.com",
+		],
+	},
 });
